aee08a939c3f7b6df9ff747cac972b6457da878c
Tomáš Szeruda <104353035+Mirlok3@users.noreply.github.com>
2023-04-20 11:47:06 +0200

    2 - Show data on page

--- index.js
+++ index.js
@@ -7,28 +7,10 @@ app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 //app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-  res.render("index");
-});
+app.get('/', async (req, res) => {
+	const set = await axios.get('http://www.karvina.cz/opendata.json');
 
-app.get('/data', async (req, res) => {
-  const set = await axios.get('http://www.karvina.cz/opendata.json');
-
-	res.write('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>');
-
-  for (obj of set.data.informace) {
-  	res.write('<div>');
-  		res.write('<h3>');
-	  		res.write(obj['název'].cs);
-  		res.write('</h3>');
-	  	res.write('<p>');
-	  		res.write(obj['typ'][1]);
-	  	res.write('<p>');
-  	res.write('</div>');
-  }
-
-  console.log(set.data);
-	res.end();
+	res.render('index', {json: set.data.informace});
 });
 
 app.get('/adresa', function(req, res) {
@@ -39,16 +21,4 @@ app.get('/about', function(req, res) {
   res.render("about");
 });
 
-
 app.listen(port);
-
-/*
-app.get('/adress', function(req, res) {
-	const currentTime = new Date();
-	const hours = currentTime.getHours();
-	const minutes = currentTime.getMinutes();
-	const seconds = currentTime.getSeconds();
-
-	const timeString = `${hours}:${minutes}:${seconds}`;
-  	res.send(`The current time is ${timeString}`);
-});*/
