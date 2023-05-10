1260723d2b7e5baf92d45b1d8b40572b81b80754
Mirlok3 <pherafixx@gmail.com>
2023-05-10 22:08:32 +0200

    4 - Finishing touches
    - Better responsive design
    - Removed styles that do nothing

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
-const axios = require('axios')
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
-//app.use(express.static('public'))
 
 app.get('/', async (req, res) => {
     const set = await axios.get('http://www.karvina.cz/opendata.json');
-
     res.render('index', {json: set.data.informace});
 });
 
